c715cfadecdd14d0eb3ace92078e42d88225ad7b
rrosegregoryy <71149194+rrosegregoryy@users.noreply.github.com>
2020-09-11 20:32:08 +0200

    Add files via upload

---
+++ drawer.js
@@ -0,0 +1,19 @@
+const canvasTag = document.querySelector('canvas')
+
+canvasTag.width = window.innerWidth * 2
+canvasTag.height = window.innerHeight * 2
+
+canvasTag.style.width = window.innerWidth + 'px'
+canvasTag.style.height = window.innerHeight + 'px'
+
+const context = canvasTag.getContext('2d')
+context.scale(2, 2)
+
+const image = document.createElement('img')
+image.src = '600px_poster3.jpg'
+
+document.addEventListener('mousemove', function(event) {
+  if (image.complete) {
+    context.drawImage(image, event.pageX, event.pageY, 400, 600)
+  }
+})
